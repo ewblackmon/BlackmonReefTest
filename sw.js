348687e4cadef159df76fd39a61ce4796fd374b4
Eric Blackmon <ewblackmon@gmail.com>
2026-01-18 10:28:56 -0800

    Make the app a PWA

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'reef-master-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './icon.png',
+    './manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Fetch Event (Offline Capability)
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
